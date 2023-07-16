82f79851111733479fa68f2c50010a23a399a8dc
Brandon Wright <brandonfullstack@gmail.com>
2023-07-15 20:37:36 -0400

    Add prices to UI

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,15 @@ function Pizza(toppings, size) {
   this.size = size;
 }
 
-Pizza.prototype.calculateCost = function() {
+Pizza.prototype.calculateCost = function () {
   const basePrice = 10;
   const toppingPrice = 3;
   const sizePrice = {
-    "small": 5,
-    "medium": 7,
-    "large": 9
+    "small": 0,
+    "medium": 5,
+    "large": 7,
+    "huge": 9,
+    "massive": 12
   };
 
   let cost = basePrice + (this.toppings.length * toppingPrice);
@@ -22,21 +24,21 @@ Pizza.prototype.calculateCost = function() {
 
 // UI Logic
 
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
   const form = document.getElementById("theForm");
   form.addEventListener("submit", displayToppings)
 })
 
-function displayToppings(event){
+function displayToppings(event) {
   event.preventDefault();
   const selectedToppings = []
 
   const pizzaSize = document.getElementById("pizzaSize").value;
   const toppingsList = document.querySelectorAll("input[name='toppings']:checked")
-  toppingsList.forEach(function(topping){
+  toppingsList.forEach(function (topping) {
     selectedToppings.push(topping.value)
   })
   myPizza = new Pizza(selectedToppings, pizzaSize);
-  document.getElementById("result").innerText =`You selected these toppings: ${selectedToppings}`
-  document.getElementById("cost").innerText =`$ ${myPizza.calculateCost()}`
+  document.getElementById("result").innerText = `You selected a ${pizzaSize} pizza with: ${selectedToppings}`
+  document.getElementById("cost").innerText = `$ ${myPizza.calculateCost()}`
 }
